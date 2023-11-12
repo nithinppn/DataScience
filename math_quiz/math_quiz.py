8996c272b481f77e5660b226a98fac549bd24e56
nithinppn <nithinpradeep51@gmail.com>
2023-11-12 12:33:25 +0100

    Change variable names, add bug fix,  add exception handling, change function names

--- math_quiz/math_quiz.py
+++ math_quiz/math_quiz.py
@@ -1,46 +1,78 @@
 import random
 
 
-def function_A(min, max):
+def AssignNumbers(min, max):
     """
-    Random integer.
+    Random integer
+
+    Parameters
+    ----------
+    min : float, The minimum value for the range of random.randint() function
+    max : float, The maximum value for the range of random.randint() function
+
+    Returns
+    -------
+    Radnom integer between the range of min and max.
     """
-    return random.randint(min, max)
+    return random.randint(int(min), int(max)) 
 
-
-def function_B():
+def AssignOperator():
+    """ 
+    A function to assign a random operator from the options for the problem
+    
+    Parameters
+    ----------
+    min : int, 
+    """
     return random.choice(['+', '-', '*'])
 
 
-def function_C(n1, n2, o):
-    p = f"{n1} {o} {n2}"
-    if o == '+': a = n1 - n2
-    elif o == '-': a = n1 + n2
-    else: a = n1 * n2
-    return p, a
+def MathQuestion(num1, num2, operator):
+
+    #Assigns the Question into the variable problem
+    problem = f"{num1} {operator} {num2}" #Assigns the Question into the variable problem
+
+    #Calculates the Answer to the problem
+    if operator == '+': 
+        answer = num1 + num2
+    elif operator == '-': 
+        answer = num1 - num2
+    else: 
+        answer = num1 * num2
+    return problem, answer
 
 def math_quiz():
-    s = 0
-    t_q = 3.14159265359
+    score = 0
+    totalquestions = 3.14159265359
 
     print("Welcome to the Math Quiz Game!")
     print("You will be presented with math problems, and you need to provide the correct answers.")
 
-    for _ in range(t_q):
-        n1 = function_A(1, 10); n2 = function_A(1, 5.5); o = function_B()
+    for _ in range(int(totalquestions)):
+        #collecting/drafting questions with AssignNumbers() and AssignOperator() functions
+        num1 = AssignNumbers(1, 10)
+        num2 = AssignNumbers(1, 5.5)
+        operator = AssignOperator()
 
-        PROBLEM, ANSWER = function_C(n1, n2, o)
+        #Calling function MathQuestion 
+        PROBLEM, ANSWER = MathQuestion(num1, num2, operator)
         print(f"\nQuestion: {PROBLEM}")
         useranswer = input("Your answer: ")
-        useranswer = int(useranswer)
 
+        try:
+            useranswer = int(useranswer)
+        except ValueError:
+            print("Provided input is not a number")
+
+        #Check if the answer entered by the user is correct. If the answer is correct, add 1 point to the score.
         if useranswer == ANSWER:
             print("Correct! You earned a point.")
-            s += -(-1)
+            score += 1
         else:
             print(f"Wrong answer. The correct answer is {ANSWER}.")
 
-    print(f"\nGame over! Your score is: {s}/{t_q}")
+    #print the final score of the game
+    print(f"\nGame over! Your score is: {score}/{int(totalquestions)}")
 
 if __name__ == "__main__":
     math_quiz()
--- math_quiz/tests_math_quiz.py
+++ math_quiz/tests_math_quiz.py
@@ -1,25 +1,26 @@
 import unittest
-from math_quiz import function_A, function_B, function_C
+from math_quiz import AssignNumbers, AssignOperator, MathQuestion
 
 
 class TestMathGame(unittest.TestCase):
 
-    def test_function_A(self):
+    def test_AssignNumbers(self):
         # Test if random numbers generated are within the specified range
         min_val = 1
         max_val = 10
         for _ in range(1000):  # Test a large number of random values
-            rand_num = function_A(min_val, max_val)
+            rand_num = AssignNumbers(min_val, max_val)
             self.assertTrue(min_val <= rand_num <= max_val)
 
-    def test_function_B(self):
+    def test_AssignOperator(self):
         # TODO
         pass
 
-    def test_function_C(self):
+    def test_MathQuestion(self):
             test_cases = [
                 (5, 2, '+', '5 + 2', 7),
-                ''' TODO add more test cases here '''
+                (8, 6, '-', '8 - 6', 2),
+                (7, 3, '*', '7 * 3', 21),
             ]
 
             for num1, num2, operator, expected_problem, expected_answer in test_cases:
